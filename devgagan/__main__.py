3c4f31f982822e90b867fe04d7959a386c84120e
vkk5880 <41853419+vkk5880@users.noreply.github.com>
2025-05-15 17:23:14 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -35,7 +35,7 @@ async def schedule_expiry_check():
     scheduler = await create_scheduler()
     while True:
         await scheduler.spawn(check_and_remove_expired_users())
-        await asyncio.sleep(60)  # Check every hour
+        await asyncio.sleep(300)  # Check every hour
         gc.collect()
 
 async def devggn_boot():
