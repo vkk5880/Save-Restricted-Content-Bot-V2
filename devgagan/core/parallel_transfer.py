16386c29c49a97baa9ab31ab89f01f9bcc0dac9b
vkk5880 <41853419+vkk5880@users.noreply.github.com>
2025-05-20 06:55:01 +0530

    Update parallel_transfer.py

--- devgagan/core/parallel_transfer.py
+++ devgagan/core/parallel_transfer.py
@@ -2,7 +2,7 @@
 > Based on parallel_file_transfer.py from mautrix-telegram, with permission to distribute under the MIT license
 > Copyright (C) 2019 Tulir Asokan - https://github.com/tulir/mautrix-telegram
 """
-import os
+from os import getenv
 import asyncio
 import hashlib
 import inspect
